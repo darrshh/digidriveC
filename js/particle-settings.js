e6a9fae7cf971f3ae622fda88bc130556ad1c9b5
darrshh <Herezeon@gmail.com>
2022-03-17 17:01:35 +0530

    fourth commit

--- js/particle-settings.js
+++ js/particle-settings.js
@@ -55,7 +55,7 @@ particlesJS("particles-js", {
 });
 var count_particles, stats, update;
 stats = new Stats();
-stats.setMode(2);
+stats.setMode(0);
 stats.domElement.style.position = "absolute";
 stats.domElement.style.left = "0px";
 stats.domElement.style.top = "0px";
